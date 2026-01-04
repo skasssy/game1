342f4897c9e79984776c372405a0463132a1486e
Скибинская Ксения <ks.skibinskaya@mail.ru>
2026-01-04 13:30:59 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            bg1 = new PictureBox();
+            bg2 = new PictureBox();
+            timer = new System.Windows.Forms.Timer(components);
+            player = new PictureBox();
+            enemy1 = new PictureBox();
+            enemy2 = new PictureBox();
+            labelLose = new Label();
+            btnRestart = new Button();
+            labelPause = new Label();
+            labelCoins = new Label();
+            labelLives = new Label();
+            coin = new PictureBox();
+            labelLevel = new Label();
+            labelHighScore = new Label();
+            ((System.ComponentModel.ISupportInitialize)bg1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bg2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)enemy1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)enemy2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)coin).BeginInit();
+            SuspendLayout();
+            // 
+            // bg1
+            // 
+            bg1.Image = (Image)resources.GetObject("bg1.Image");
+            bg1.Location = new Point(0, 0);
+            bg1.Name = "bg1";
+            bg1.Size = new Size(840, 650);
+            bg1.TabIndex = 0;
+            bg1.TabStop = false;
+            // 
+            // bg2
+            // 
+            bg2.Image = (Image)resources.GetObject("bg2.Image");
+            bg2.Location = new Point(0, -650);
+            bg2.Name = "bg2";
+            bg2.Size = new Size(840, 650);
+            bg2.TabIndex = 1;
+            bg2.TabStop = false;
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 15;
+            timer.Tick += timer_Tick;
+            // 
+            // player
+            // 
+            player.BackColor = Color.FromArgb(64, 64, 64);
+            player.Image = (Image)resources.GetObject("player.Image");
+            player.Location = new Point(296, 517);
+            player.Name = "player";
+            player.Size = new Size(128, 128);
+            player.SizeMode = PictureBoxSizeMode.StretchImage;
+            player.TabIndex = 2;
+            player.TabStop = false;
+            // 
+            // enemy1
+            // 
+            enemy1.BackColor = Color.FromArgb(64, 64, 64);
+            enemy1.Image = (Image)resources.GetObject("enemy1.Image");
+            enemy1.Location = new Point(172, -130);
+            enemy1.Name = "enemy1";
+            enemy1.Size = new Size(128, 128);
+            enemy1.SizeMode = PictureBoxSizeMode.StretchImage;
+            enemy1.TabIndex = 3;
+            enemy1.TabStop = false;
+            // 
+            // enemy2
+            // 
+            enemy2.BackColor = Color.FromArgb(64, 64, 64);
+            enemy2.Image = (Image)resources.GetObject("enemy2.Image");
+            enemy2.Location = new Point(552, -400);
+            enemy2.Name = "enemy2";
+            enemy2.Size = new Size(128, 128);
+            enemy2.SizeMode = PictureBoxSizeMode.StretchImage;
+            enemy2.TabIndex = 4;
+            enemy2.TabStop = false;
+            // 
+            // labelLose
+            // 
+            labelLose.AutoSize = true;
+            labelLose.BackColor = Color.Red;
+            labelLose.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelLose.ForeColor = Color.White;
+            labelLose.Location = new Point(243, 158);
+            labelLose.Name = "labelLose";
+            labelLose.Size = new Size(360, 54);
+            labelLose.TabIndex = 5;
+            labelLose.Text = "ВЫ ПРОИГРАЛИ!";
+            // 
+            // btnRestart
+            // 
+            btnRestart.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            btnRestart.Location = new Point(340, 235);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(161, 42);
+            btnRestart.TabIndex = 6;
+            btnRestart.Text = "Перезапустить";
+            btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Click += btnRestart_Click;
+            // 
+            // labelPause
+            // 
+            labelPause.AutoSize = true;
+            labelPause.BackColor = Color.Red;
+            labelPause.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            labelPause.ForeColor = Color.White;
+            labelPause.Location = new Point(24, 601);
+            labelPause.Name = "labelPause";
+            labelPause.Size = new Size(78, 28);
+            labelPause.TabIndex = 7;
+            labelPause.Text = "ПАУЗА";
+            labelPause.Visible = false;
+            // 
+            // labelCoins
+            // 
+            labelCoins.AutoSize = true;
+            labelCoins.BackColor = Color.White;
+            labelCoins.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelCoins.ForeColor = Color.Black;
+            labelCoins.Location = new Point(24, 23);
+            labelCoins.Name = "labelCoins";
+            labelCoins.Size = new Size(111, 28);
+            labelCoins.TabIndex = 8;
+            labelCoins.Text = "Монеты: 0";
+            // 
+            // labelLives
+            // 
+            labelLives.AutoSize = true;
+            labelLives.BackColor = Color.White;
+            labelLives.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelLives.ForeColor = Color.Black;
+            labelLives.Location = new Point(702, 23);
+            labelLives.Name = "labelLives";
+            labelLives.Size = new Size(128, 28);
+            labelLives.TabIndex = 9;
+            labelLives.Text = "Жизни: ♥♥♥";
+            // 
+            // coin
+            // 
+            coin.BackColor = Color.FromArgb(64, 64, 64);
+            coin.Image = (Image)resources.GetObject("coin.Image");
+            coin.Location = new Point(565, -600);
+            coin.Name = "coin";
+            coin.Size = new Size(32, 32);
+            coin.SizeMode = PictureBoxSizeMode.StretchImage;
+            coin.TabIndex = 10;
+            coin.TabStop = false;
+            // 
+            // labelLevel
+            // 
+            labelLevel.AutoSize = true;
+            labelLevel.BackColor = Color.White;
+            labelLevel.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelLevel.ForeColor = Color.Black;
+            labelLevel.Location = new Point(702, 75);
+            labelLevel.Name = "labelLevel";
+            labelLevel.Size = new Size(111, 28);
+            labelLevel.TabIndex = 11;
+            labelLevel.Text = "Уровень: 1";
+            // 
+            // labelHighScore
+            // 
+            labelHighScore.AutoSize = true;
+            labelHighScore.BackColor = Color.White;
+            labelHighScore.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelHighScore.ForeColor = Color.Black;
+            labelHighScore.Location = new Point(702, 601);
+            labelHighScore.Name = "labelHighScore";
+            labelHighScore.Size = new Size(103, 28);
+            labelHighScore.TabIndex = 12;
+            labelHighScore.Text = "Рекорд: 0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(840, 650);
+            Controls.Add(labelHighScore);
+            Controls.Add(labelLevel);
+            Controls.Add(coin);
+            Controls.Add(labelLives);
+            Controls.Add(labelCoins);
+            Controls.Add(labelPause);
+            Controls.Add(btnRestart);
+            Controls.Add(labelLose);
+            Controls.Add(enemy2);
+            Controls.Add(enemy1);
+            Controls.Add(player);
+            Controls.Add(bg1);
+            Controls.Add(bg2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyPress += Form1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)bg1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bg2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)enemy1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)enemy2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)coin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox bg1;
+        private PictureBox bg2;
+        private System.Windows.Forms.Timer timer;
+        private PictureBox player;
+        private PictureBox enemy1;
+        private PictureBox enemy2;
+        private Label labelLose;
+        private Button btnRestart;
+        private Label labelPause;
+        private Label labelCoins;
+        private Label labelLives;
+        private PictureBox coin;
+        private Label labelLevel;
+        private Label labelHighScore;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        private Point pos;
+        private bool dragging, lose = false;
+        private int countCoins = 0;
+        private int lives = 3;
+        private int level = 1;
+        private int highScore = 0;
+        private bool isInvulnerable = false;
+        private int invulnerabilityTimer = 0;
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bg1.MouseDown += MouseClickDown;
+            bg1.MouseUp += MouseClickUp;
+            bg1.MouseMove += MouseClickMove;
+
+            bg2.MouseDown += MouseClickDown;
+            bg2.MouseUp += MouseClickUp;
+            bg2.MouseMove += MouseClickMove;
+
+            // Инициализация UI
+            labelLose.Visible = false;
+            btnRestart.Visible = false;
+            KeyPreview = true;
+
+            // Инициализация жизней
+            UpdateLivesDisplay();
+        }
+
+        private void MouseClickDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            pos.X = e.X;
+            pos.Y = e.Y;
+        }
+
+        private void MouseClickUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void MouseClickMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point currPoint = PointToScreen(new Point(e.X, e.Y));
+                this.Location = new Point(currPoint.X - pos.X, currPoint.Y - pos.Y + bg1.Top);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+            else if (e.KeyChar == 'p' || e.KeyChar == 'P')
+                TogglePause();  // Пауза по клавише P
+        }
+
+        private void TogglePause()
+        {
+            timer.Enabled = !timer.Enabled;
+            if (Controls.ContainsKey("labelPause"))
+            {
+                labelPause.Visible = !timer.Enabled;
+            }
+        }
+
+        private void ResetCoin()
+        {
+            coin.Top = -50;
+            coin.Left = rand.Next(150, 560);
+        }
+
+        private void ResetEnemy(PictureBox enemy, int minLeft, int maxLeft)
+        {
+            enemy.Top = -rand.Next(100, 400);
+            enemy.Left = rand.Next(minLeft, maxLeft);
+        }
+
+        private void CheckCollisions()
+        {
+            // Проверяем столкновение с врагами
+            if ((player.Bounds.IntersectsWith(enemy1.Bounds) ||
+                 player.Bounds.IntersectsWith(enemy2.Bounds)) && !isInvulnerable)
+            {
+                HandleCollision();
+            }
+        }
+
+        private void HandleCollision()
+        {
+            player.BackColor = Color.Red;
+
+            // Потеря жизни
+            lives--;
+            UpdateLivesDisplay();
+
+            if (lives <= 0)
+            {
+                // Конец игры
+                GameOver();
+            }
+            else
+            {
+                isInvulnerable = true;
+                invulnerabilityTimer = 40;
+            }
+        }
+
+        private void CollectCoin()
+        {
+            countCoins++;
+            labelCoins.Text = "Монеты: " + countCoins.ToString();
+
+            // Обновляем рекорд
+            if (countCoins > highScore)
+            {
+                highScore = countCoins;
+                if (Controls.ContainsKey("labelHighScore"))
+                {
+                    labelHighScore.Text = "Рекорд: " + highScore.ToString();
+                }
+            }
+
+            // Проверка уровня
+            CheckLevelUp();
+
+            ResetCoin();
+        }
+
+        private void CheckLevelUp()
+        {
+            int newLevel = 1 + (countCoins / 5); // Новый уровень каждые 5 монет
+
+            if (newLevel > level)
+            {
+                level = newLevel;
+
+                // Обновляем отображение уровня
+                if (Controls.ContainsKey("labelLevel"))
+                {
+                    labelLevel.Text = "Уровень: " + level.ToString();
+                }
+
+                if (Controls.ContainsKey("labelCoins"))
+                {
+                    labelCoins.ForeColor = Color.Green;
+                }
+            }
+        }
+
+        private void UpdateLivesDisplay()
+        {
+            if (Controls.ContainsKey("labelLives"))
+            {
+                labelLives.Text = "Жизни: " + new string('❤', lives);
+                labelLives.ForeColor = lives == 1 ? Color.Red :
+                                      lives == 2 ? Color.Orange :
+                                      Color.Green;
+            }
+        }
+
+        private void GameOver()
+        {
+            timer.Enabled = false;
+            labelLose.Visible = true;
+            btnRestart.Visible = true;
+            lose = true;
+        }
+
+        private void ResetGame()
+        {
+            // Остановка таймера
+            timer.Enabled = false;
+
+            // Сброс переменных
+            lose = false;
+            countCoins = 0;
+            lives = 3;
+            level = 1;
+            isInvulnerable = false;
+            invulnerabilityTimer = 0;
+
+            // Сброс скорости таймера
+            timer.Interval = 50;
+
+            // Сброс UI элементов
+            labelLose.Visible = false;
+            btnRestart.Visible = false;
+
+            if (Controls.ContainsKey("labelPause"))
+            {
+                labelPause.Visible = false;
+            }
+
+            // Сброс отображения
+            labelCoins.Text = "Монеты: 0";
+            labelCoins.ForeColor = SystemColors.ControlText;
+
+            if (Controls.ContainsKey("labelLevel"))
+            {
+                labelLevel.Text = "Уровень: 1";
+            }
+
+            if (Controls.ContainsKey("labelHighScore"))
+            {
+                labelHighScore.Text = "Рекорд: " + highScore.ToString();
+            }
+
+            UpdateLivesDisplay();
+
+            // Сброс позиции игрока
+            player.Left = 300;
+            player.Top = 500;
+            player.BackColor = Color.Transparent;
+
+            // Сброс позиций врагов
+            enemy1.Top = -130;
+            enemy2.Top = -400;
+
+            // Сброс позиции монеты
+            ResetCoin();
+
+            // Включение таймера
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Если игра на паузе или проиграна - выходим
+            if (!timer.Enabled || lose) return;
+
+            // Обновляем таймер неуязвимости
+            if (isInvulnerable)
+            {
+                invulnerabilityTimer--;
+                if (invulnerabilityTimer <= 0)
+                {
+                    isInvulnerable = false;
+                    player.BackColor = Color.Transparent;
+                }
+                else
+                {
+                    // Мигание при неуязвимости
+                    player.BackColor = (invulnerabilityTimer / 5) % 2 == 0 ? Color.Transparent : Color.FromArgb(150, Color.Yellow);
+                }
+            }
+
+            int bgSpeed = 1 + level;  // Скорость фона
+            int enemySpeed = 3 + level * 2;  // Скорость врагов
+
+            // Движение фона5
+            bg1.Top += bgSpeed;
+            bg2.Top += bgSpeed;
+
+            if (bg1.Top >= 650)
+            {
+                bg1.Top = 0;
+                bg2.Top = -650;
+            }
+
+            // Движение врагов
+            enemy1.Top += enemySpeed;
+            enemy2.Top += enemySpeed;
+
+            // Движение монеты
+            coin.Top += bgSpeed;
+
+
+            // Восстановление объектов за пределами экрана
+            if (coin.Top >= 650)
+            {
+                ResetCoin();
+            }
+
+            if (enemy1.Top >= 650)
+            {
+                ResetEnemy(enemy1, 150, 300);
+            }
+
+            if (enemy2.Top >= 650)
+            {
+                ResetEnemy(enemy2, 300, 560);
+            }
+
+            // Проверка столкновений с врагами
+            CheckCollisions();
+
+            // Проверка сбора монеты
+            if (player.Bounds.IntersectsWith(coin.Bounds))
+            {
+                CollectCoin();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            {
+                if (lose || !timer.Enabled) return;
+
+                int speed = 10;
+                bool moved = false;
+
+                // Основное управление
+                if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && player.Left > 150)
+                {
+                    player.Left -= speed;
+                    moved = true;
+                }
+                else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && player.Right < 700)
+                {
+                    player.Left += speed;
+                    moved = true;
+                }
+
+
+                // Вертикальное движение
+                if ((e.KeyCode == Keys.Up || e.KeyCode == Keys.W) && player.Top > 0)
+                {
+                    player.Top -= speed;
+                    moved = true;
+                }
+                else if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.S) && player.Bottom < 650)
+                {
+                    player.Top += speed;
+                    moved = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.BackColor = Color.Transparent;
+            if (Controls.ContainsKey("labelPause"))
+            {
+                labelPause.Visible = false;
+            }
+
+            // Инициализируем отображение рекорда
+            if (Controls.ContainsKey("labelHighScore"))
+            {
+                labelHighScore.Text = "Рекорд: " + highScore.ToString();
+            }
+
+            // Инициализируем отображение уровня
+            if (Controls.ContainsKey("labelLevel"))
+            {
+                labelLevel.Text = "Уровень: " + level.ToString();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
